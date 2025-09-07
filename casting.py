592f42741dff7d2760d2cbd4d4e687fa719c16ea
Sumit-Shastri <shastrisumit25@gmail.com>
2025-09-07 19:39:14 +0530

    casting in python

---
+++ casting.py
@@ -0,0 +1,36 @@
+# Casting is converting from one datatype to diferent
+
+# int() casting - converting other types to int
+
+x = int(1)  # --> 1
+y = int(2.987)  # --> 2
+z = int("3")  # str to int --> 3
+
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+
+
+# float()  casting - converting others to float
+
+a = float(1)
+b = float(2.8)
+c = float("4")
+d = float("5.6")
+
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+
+
+# str()
+
+p = str("s1")
+q = str(2)
+r = str(3.0)
+
+print(p,type(p))
+print(q,type(q))
+print(r,type(r))
+
