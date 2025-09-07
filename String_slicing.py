6ca7d695f30a87bf89a1972ec948899a81ba106d
Sumit-Shastri <shastrisumit25@gmail.com>
2025-09-07 21:48:36 +0530

    String Slicing learn

---
+++ String_slicing.py
@@ -0,0 +1,23 @@
+# Returing range of characters from a string
+
+#EX.
+
+#here 2nd parameter is not included
+
+b = "Hello , World !"
+print(b[0:4])
+
+
+# Slicing from the Start
+
+print(b[:5])
+
+
+# Slice to End
+
+print(b[2:])
+
+
+# Negative slicing
+
+print(b[-5:-2])
