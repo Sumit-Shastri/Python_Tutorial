2f7dcd18bddfa1d74656800f1ab3fb914c919056
Sumit-Shastri <shastrisumit25@gmail.com>
2025-09-07 22:19:50 +0530

    String concatenate learn

---
+++ Strings_Contatenation.py
@@ -0,0 +1,18 @@
+# Concatenating 2 strings
+
+str1 = "Sumit"
+str2 = "Shastri"
+
+# Concatenating str1 + str2
+
+str3 = str1 + str2
+
+print(str3)
+
+
+# Concatenating with spaces
+
+str4 = str1 + " " + str2
+
+print(str4)
+
